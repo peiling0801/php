58d0d4e9cd84498849aaadc4a0e1886eae851c9d
peiling0801 <97140304+peiling0801@users.noreply.github.com>
2025-04-09 00:08:20 +0800

    Update 5.while.php

--- 5.while.php
+++ 5.while.php
@@ -1,1 +1,14 @@
+<?php
+// 5.while.php - 使用 while 迴圈列出 user 資料表中所有帳號與密碼
 
+// 建立與資料庫的連線
+$conn = mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
+
+// 執行 SQL 查詢：從 user 資料表取出所有資料
+$result = mysqli_query($conn, "SELECT * FROM user");
+
+// 使用 while 迴圈逐筆讀取資料列
+while ($row = mysqli_fetch_array($result)) {
+    echo $row["id"] . " " . $row["pwd"] . "<br>";
+}
+?>
